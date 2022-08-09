b419fc0ca0859b1e32238654885868ec584a7dec
Gavin Douch <gjdouch@me.com>
2022-08-09 22:08:57 +1000

    * Replaced party choice with student/company picker

---
+++ assets/scripts/callToActionButton.js
@@ -0,0 +1,21 @@
+---
+---
+const toggle = () => {
+	if (document.getElementById("call-to-action-word").innerText === "Student") {
+		document.getElementById("call-to-action-chevron").style.rotate = "-180deg";
+		document.getElementById("call-to-action").children[0].style.width = "195px";
+		document.getElementById("call-to-action-looking-for").innerText = "good students";
+		document.getElementById("call-to-action-go").href = "javascript:delay('{{ '/companies' | relative_url }}')";
+
+		setTimeout( function() {
+			document.getElementById("call-to-action-word").innerText = "Company";
+		}, 80 );
+	}
+	else {
+		document.getElementById("call-to-action-word").innerText = "Student";
+		document.getElementById("call-to-action-looking-for").innerText = "experience";
+		document.getElementById("call-to-action-chevron").style.rotate = "0deg";
+		document.getElementById("call-to-action").children[0].style.width = "156px";
+		document.getElementById("call-to-action-go").href = "javascript:delay('{{ '/students' | relative_url }}')";
+	}
+}
